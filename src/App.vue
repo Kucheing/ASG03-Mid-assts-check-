4ed77bf195249a1b1677811f890bdf42002ac563
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-13 16:41:02 +0800

    Update font styles in App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img style="border-radius: 5%;" src="./assets/images/student.jpeg" class="logo cream-border"/>
-  <h1 class="text-3xl font-bold underline sapphire">Izzul Faris Zulkiffli</h1>
+  <h1 class="text-3xl font-bold font-pacifico underline sapphire">Izzul Faris Zulkiffli</h1>
 <br>
 <br>
-    <h1 class="text-2xl font-sans-serif lightgreentext">Full Name: Mohammad Izzul Faris Bin Haji Zulkiffli</h1>
-  <h1 class="text-2xl font-sans-serif margin-right redtext">Student ID: 22FTT1517</h1>
-  <h1 class="text-2xl font-sans-serif lightgreentext">Programme: Web Development</h1>
-  <h1 class="text-2xl font-sans-serif redtext">Age: 19 Years Old</h1>
-  <h1 class="text-2xl font-sans-serif lightgreentext">School: Politeknik Brunei School of Information, Communication and Technology</h1>
-  <h1 class="text-2xl font-sans-serif redtext">Email: fariszlk.com@gmail.com</h1>
+    <h1 class="text-2xl font-beautiful lightgreentext">Full Name: Mohammad Izzul Faris Bin Haji Zulkiffli</h1>
+  <h1 class="text-2xl font-kalam margin-right redtext">Student ID: 22FTT1517</h1>
+  <h1 class="text-2xl font-beautiful lightgreentext">Programme: Web Development</h1>
+  <h1 class="text-2xl font-kalam redtext">Age: 19 Years Old</h1>
+  <h1 class="text-2xl font-beautiful lightgreentext">School: Politeknik Brunei School of Information, Communication and Technology</h1>
+  <h1 class="text-2xl font-kalam redtext">Email: fariszlk.com@gmail.com</h1>
 </template>
 
 <style scoped>
@@ -21,6 +21,17 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
   margin-left: 175px
+}
+.font-kalam{
+  font-family: 'Kalam', monospace;
+}
+
+.font-pacifico{
+  font-family: 'Pacifico', fantasy;
+}
+
+.font-beautiful{
+  font-family: 'Beautiful People', serif;
 }
 
 .cream-border{
