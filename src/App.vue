db074f8f4b8e7d800d04218b6f41f7717509aa54
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-13 16:23:34 +0800

    Updated to student profile with information and datas

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img src="./assets/images/student.jpeg" class="logo align-center"/>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <HelloWorld msg="Vite + Vue" />
+  <img style="border-radius: 5%;" src="./assets/images/student.jpeg" class="logo"/>
+  <h1 class="text-3xl font-bold underline">Izzul Faris Zulkiffli</h1>
+<br>
+<br>
+    <h1 class="text-2xl font-sans-serif">Full Name: Mohammad Izzul Faris Bin Haji Zulkiffli</h1>
+  <h1 class="text-2xl font-sans-serif margin-right">Student ID: 22FTT1517</h1>
+  <h1 class="text-2xl font-sans-serif">Programme: Web Development</h1>
+  <h1 class="text-2xl font-sans-serif">Age: 19 Years Old</h1>
+  <h1 class="text-2xl font-sans-serif">School: Politeknik Brunei School of Information, Communication and Technology</h1>
+
 </template>
 
 <style scoped>
@@ -13,7 +20,11 @@ import HelloWorld from './components/HelloWorld.vue'
   height: 30em;
   will-change: filter;
   transition: filter 300ms;
-  margin-left: 50px
+  margin-left: 170px
+}
+
+.margin-right{
+  margin-right: 10px;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #ffffff);
