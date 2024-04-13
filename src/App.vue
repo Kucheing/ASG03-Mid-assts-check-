0f6753cece5410e0abe324395f0f00eca1d9a1ab
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-13 16:33:27 +0800

    Giving colours and styles

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,16 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img style="border-radius: 5%;" src="./assets/images/student.jpeg" class="logo"/>
-  <h1 class="text-3xl font-bold underline">Izzul Faris Zulkiffli</h1>
+  <img style="border-radius: 5%;" src="./assets/images/student.jpeg" class="logo cream-border"/>
+  <h1 class="text-3xl font-bold underline sapphire">Izzul Faris Zulkiffli</h1>
 <br>
 <br>
-    <h1 class="text-2xl font-sans-serif">Full Name: Mohammad Izzul Faris Bin Haji Zulkiffli</h1>
-  <h1 class="text-2xl font-sans-serif margin-right">Student ID: 22FTT1517</h1>
-  <h1 class="text-2xl font-sans-serif">Programme: Web Development</h1>
-  <h1 class="text-2xl font-sans-serif">Age: 19 Years Old</h1>
-  <h1 class="text-2xl font-sans-serif">School: Politeknik Brunei School of Information, Communication and Technology</h1>
-
+    <h1 class="text-2xl font-sans-serif lightgreentext">Full Name: Mohammad Izzul Faris Bin Haji Zulkiffli</h1>
+  <h1 class="text-2xl font-sans-serif margin-right redtext">Student ID: 22FTT1517</h1>
+  <h1 class="text-2xl font-sans-serif lightgreentext">Programme: Web Development</h1>
+  <h1 class="text-2xl font-sans-serif redtext">Age: 19 Years Old</h1>
+  <h1 class="text-2xl font-sans-serif lightgreentext">School: Politeknik Brunei School of Information, Communication and Technology</h1>
+  <h1 class="text-2xl font-sans-serif redtext">Email: fariszlk.com@gmail.com</h1>
 </template>
 
 <style scoped>
@@ -20,14 +20,30 @@ import HelloWorld from './components/HelloWorld.vue'
   height: 30em;
   will-change: filter;
   transition: filter 300ms;
-  margin-left: 170px
+  margin-left: 175px
 }
+
+.cream-border{
+  border: 3px solid #fffdd0;
+}
+
+.sapphire{
+  color: #0f52ba;
+}
+
+.redtext{
+  color: #ff0000;
+}
+
+.lightgreentext{
+  color: #8abb86;
+  }
 
 .margin-right{
   margin-right: 10px;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #ffffff);
+  filter: drop-shadow(0 0 2em #fffdd0);
 }
 
 </style>
