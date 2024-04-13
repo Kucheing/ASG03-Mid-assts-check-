b1fcd8e0b90ed15bc5f969c35115c0f6f221ce05
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-13 16:08:42 +0800

    Add Student image and hover effect to the image

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,20 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world</h1>
+  <img src="./assets/images/student.jpeg" class="logo align-center"/>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.logo {
+  height: 30em;
+  will-change: filter;
+  transition: filter 300ms;
+  margin-left: 50px
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #ffffff);
+}
+
 </style>
