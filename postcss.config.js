6d11f0e388b77dcac9713b525594e059a9961695
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-14 10:21:58 +0800

    Add PostCSS configuration for Tailwind CSS and PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,18 @@ export default {
     autoprefixer: {},
   },
 }
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    process.env.NODE_ENV === 'production'
+      ? require('@fullhuman/postcss-purgecss')({
+          content: [
+            './public/**/*.html',
+            './src/**/*.vue'
+          ],
+          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
+      : ''
+  ]
+}
