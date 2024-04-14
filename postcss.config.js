c69c08f4f1f94c359fbcbaab160cf237d9fd7ce4
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-14 10:24:05 +0800

    Remove unused PostCSS plugins

--- postcss.config.js
+++ postcss.config.js
@@ -4,18 +4,3 @@ export default {
     autoprefixer: {},
   },
 }
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    process.env.NODE_ENV === 'production'
-      ? require('@fullhuman/postcss-purgecss')({
-          content: [
-            './public/**/*.html',
-            './src/**/*.vue'
-          ],
-          defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        })
-      : ''
-  ]
-}
