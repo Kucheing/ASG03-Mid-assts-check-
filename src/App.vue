d425ba9e3f4b29a717cdd3adc83c3665261286c4
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-15 19:37:03 +0800

    rework all the styling making everything that is able to be implemented into tailwind css possible

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@ import HelloWorld from './components/HelloWorld.vue'
 <template style="width:100%">
 <div class="flex-wrap md:flex" style="width:100%">
     <div>
-  <img src="./assets/images/student.jpeg" class="align-center1 containerpic rounded-full w-auto h-auto logo object-cover border4r"/>
-  <h1 class="text-4xl font-semibold creamtext marg1 align-center1">Izzul Faris Zulkiffli</h1>
+  <img src="./assets/images/student.jpeg" class="border-4 border-solid border-red-700 h-64 w-64 ml-40 mb-8 flex justify-center items-center rounded-full lg:h-128 lg:w-128 lg:border-4 lg:border-solid lg:border-yellow-100 logo object-cover "/>
+  <h1 class="text-4xl text-red-700 font-bold text-center  lg:mt-20 lg:text-4xl lg:ml-40 lg:text-yellow-100">Izzul Faris Zulkiffli</h1>
     </div>
     <div class="flex-grow">
-  <h1 class="text-2xl align-center font-bold creamtext marg2 align-center1">Mohammad Izzul Faris Bin Haji Zulkiffli, 22FTT1517, DWDT08</h1>
-  <br> <br> <br> <br> <br>  <br> 
-  <h1 class="text-2xl align-center creamtext marg2 align-center1">Currently, I'm pursuing a Level 5 Diploma in Web Development at 
+  <h1 class="text-4xl text-red-700 text-center font-bold lg:mt-20 lg:text-4xl lg:ml-40 lg:text-yellow-100">Mohammad Izzul Faris Bin Haji Zulkiffli, 22FTT1517, DWDT08</h1>
+  <br> <br> <br> <br>
+  <h1 class="text-4xl text-red-700 font-semibold text-center lg:text-3xl lg:ml-40 lg:text-yellow-100">Currently, I'm pursuing a Level 5 Diploma in Web Development at 
   <br> Politeknik Brunei. When I'm not immersed in coding, 
   <br> you can find me cycling through scenic routes, 
   <br> diving into the captivating world of anime, or enjoying a 
@@ -20,18 +20,16 @@ import HelloWorld from './components/HelloWorld.vue'
   <br> Balancing my passion for technology with a vibrant array 
   <br> of hobbies keeps me inspired and energized.</h1>
 
-
   <br>
   <br>
   <br>
   <br> 
   <br>
 
-  
-  <h1 class="text-2xl font-bold creamtext marg2 align-center align-center1">CONTACTS:</h1>
-  <h1 class="text-2xl creamtext marg2 align-center align-center1">22ftt1517@student.pb.edu.bn</h1>
-  <h1 class="text-2xl creamtext marg2 align-center align-center1">+673 xxx xxxx</h1>
-  <h1 class="text-2xl creamtext marg2 align-center align-center1">www.faris.io</h1>
+  <h1 class="text-4xl text-red-700 font-bold text-center lg:text-4xl lg:ml-40 lg:text-yellow-100">CONTACTS:</h1>
+  <h1 class="text-4xl text-red-700 text-center lg:text-3xl lg:ml-40 lg:text-yellow-100">22ftt1517@student.pb.edu.bn</h1>
+  <h1 class="text-4xl text-red-700 text-center lg:text-3xl lg:ml-40 lg:text-yellow-100">+673 xxx xxxx</h1>
+  <h1 class="text-4xl text-red-700 text-center lg:text-3xl lg:ml-40 lg:text-yellow-100">www.faris.io</h1>
   </div>
 </div>
 
@@ -43,67 +41,26 @@ import HelloWorld from './components/HelloWorld.vue'
 /* For mobile view */
 @media (max-width: 768px) {
 
-.containerpic {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-.border4r{
-border: 4px solid #ff0000;
-  }
-
-.creamtext{
-  color: #ff0000;
-  }
-
-.align-center1{
-  text-align: center;
-  }
+.border4r {
+border-width: 4px;
+border-style: solid;
+border-color: red;
+}
 
 .logo {
-  height: 250px;
-  width: 250px;
   will-change: filter;
-  transition: filter 300ms;
-  margin-left: 150px;
-  margin-bottom: 30px;
-  }
-
-.container {
-    display: flex;
-    flex-wrap: wrap;
-  }
+  transition: filter 300ms;}
 
 .logo:hover {
   filter: drop-shadow(0 0 2em #ff0000);
   }
 }
 
-
 /* For desktop view */
-@media (min-width: 1600px) {
-
+@media (min-width: 1024px) {
 h1 {
     white-space: nowrap;
     overflow: visible;
-  }
-  
-.align-center {
-    text-align: center;
-  }
-
-.marg1 {
-margin-top: 10%;
-margin-left: 42%;
-  }
-
-.marg2{
-margin-left: 15%;
-  }
-
-.creamtext {
-  color: #fffdd0;
   }
 
 .logo {
@@ -111,8 +68,6 @@ margin-left: 15%;
   width: 500px;
   will-change: filter;
   transition: filter 300ms;
-  margin-left: 175px;
-  margin-bottom: 30px;
   }
 
 .logo:hover {
