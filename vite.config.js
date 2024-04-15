420743dc955960c0c12ecc8948b79f9d4321d85c
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-15 14:04:24 +0800

    try to add hello to new updated username

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
+// Currently, I'm pursuing a Level 5 Diploma in Web Development at Politeknik Brunei. When I'm not immersed in coding, you can find me cycling through scenic routes, diving into the captivating world of anime, or enjoying a good game. I also cherish spending quality time with friends. Balancing my passion for technology with a vibrant array of hobbies keeps me inspired and energized.
