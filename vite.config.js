4d35373ee3a34d8e6baf9076cfa7ba6484270290
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-15 15:12:38 +0800

    Adding base

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,3 @@ export default defineConfig({
   base: '/ASG03-Mid-assts-check-/',
   plugins: [vue()],
 })
-// Currently, I'm pursuing a Level 5 Diploma in Web Development at Politeknik Brunei. When I'm not immersed in coding, you can find me cycling through scenic routes, diving into the captivating world of anime, or enjoying a good game. I also cherish spending quality time with friends. Balancing my passion for technology with a vibrant array of hobbies keeps me inspired and energized.
