0500a92ef1f241cb931a19ddff64bc098d293671
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-15 17:07:25 +0800

    Refactor App.vue layout and update contact information

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,22 @@ import HelloWorld from './components/HelloWorld.vue'
     <div class="flex-grow">
   <h1 class="text-2xl align-center font-bold creamtext marg2 align-center1">Mohammad Izzul Faris Bin Haji Zulkiffli, 22FTT1517, DWDT08</h1>
   <br> <br> <br> <br> <br>  <br> 
-  <h1 class="text-2xl align-center creamtext marg2 align-center1">Currently, I'm pursuing a Level 5 Diploma in Web Development at <br> Politeknik Brunei. When I'm not immersed in coding, <br> you can find me cycling through scenic routes, <br> diving into the captivating world of anime, or enjoying a <br> good game. I also cherish spending quality time with friends. <br> Balancing my passion for technology with a vibrant array <br> of hobbies keeps me inspired and energized.</h1>
+  <h1 class="text-2xl align-center creamtext marg2 align-center1">Currently, I'm pursuing a Level 5 Diploma in Web Development at 
+  <br> Politeknik Brunei. When I'm not immersed in coding, 
+  <br> you can find me cycling through scenic routes, 
+  <br> diving into the captivating world of anime, or enjoying a 
+  <br> good game. I also cherish spending quality time with friends. 
+  <br> Balancing my passion for technology with a vibrant array 
+  <br> of hobbies keeps me inspired and energized.</h1>
+
+
   <br>
   <br>
   <br>
   <br> 
   <br>
+
+  
   <h1 class="text-2xl font-bold creamtext marg2 align-center align-center1">CONTACTS:</h1>
   <h1 class="text-2xl creamtext marg2 align-center align-center1">22ftt1517@student.pb.edu.bn</h1>
   <h1 class="text-2xl creamtext marg2 align-center align-center1">+673 xxx xxxx</h1>
