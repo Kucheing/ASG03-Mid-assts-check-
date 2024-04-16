dc98da033fb01de8b3a836275637270ecb368326
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2024-04-16 08:37:46 +0800

    adding back the old export while removing the PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,30 +1,31 @@
-
-// postcss.config.js
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    process.env.NODE_ENV === 'production'
-      ? require('@fullhuman/postcss-purgecss')({
-          content: [
-            './src/**/*.vue',
-            './src/**/*.js',
-            // etc.
-          ],
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        })
-      : undefined,
-    require('autoprefixer'),
-    require('cssnano')({
-      preset: 'default',
-    }),
-  ]
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
 
-// export default {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
+// postcss.config.js
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     process.env.NODE_ENV === 'production'
+//       ? require('@fullhuman/postcss-purgecss')({
+//           content: [
+//             './src/**/*.vue',
+//             './src/**/*.js',
+//             // etc.
+//           ],
+//           defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+//         })
+//       : undefined,
+//     require('autoprefixer'),
+//     require('cssnano')({
+//       preset: 'default',
+//     }),
+//   ]
 // }
+
+
 
 
